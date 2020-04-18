9df4459b181cb7bc298d7710a3d2aafff31543a8
Tommy Anfosso <anfossotommy@hotmail.fr>
2020-04-18 04:00:38 +0200

    refactor Controller::proposerCoup OK

--- src/morpion/Controller.java
+++ src/morpion/Controller.java
@@ -10,13 +10,15 @@ class Controller {
 	
 	private Ai ai;
 
-	Controller(Ihm ihm, Ent ent) throws Exception {
+	Controller(Ihm ihm, Ent ent) throws Exception 
+	{
 		this.ihm = ihm;
 		this.ent = ent;
 		this.ai = new Ai(this.ent.getDiff());
 	}
 	
-	public void entToIhm() {
+	public void entToIhm() 
+	{
 		Grille grille = this.ent.getGrille();
 		
 		for(int i = 0 ; i < Ent.TAILLE_GRILLE ; ++i)
@@ -24,92 +26,85 @@ class Controller {
 		this.ihm.setTourDeJeu(this.ent.getTourJeu());
 	}
 	
-	public void proposerCoup(int id) throws IOException {
-
-		Case c = this.ent.getGrille().at(id);
+	public void proposerCoup(int id) throws IOException 
+	{
+		Grille grille = this.ent.getGrille();
 		
-		if(c == Case.VIDE)
+		if(grille.at(id) == Case.VIDE)
 		{
-			Grille save = new Grille(this.ent.getGrille());
-			this.ent.getGrille().set(id, Case.valueOf(this.ent.getTourJeu().toString()));
-			if(this.ent.getTourJeu() == Joueur.X)
-				this.ai.data.coupsX.add(new Ai.Data.Coup(save, new Grille(this.ent.getGrille())));
-			else
-				this.ai.data.coupsY.add(new Ai.Data.Coup(save, new Grille(this.ent.getGrille())));
-			this.incrementerTourDeJeu();
-			this.entToIhm();
-			Joueur vainqueur = this.ent.getGrille().finDePartie();
-			boolean partieTerminee = (vainqueur != null) || this.ent.getGrille().is_filled();
-			if(partieTerminee)
-			{
-				if(vainqueur != null)
-				{
-					System.out.println("Le vainqueur est " + vainqueur.toString());
-					TextFile.stringToFile(this.ai.data.getCoups(vainqueur), 
-							Ai.DATA_DIRPATH + Ai.COUPS_FILENAME, true);
-					this.ai.learn();
-				}
-				else
-					System.out.println("Aucun gagnant");
-				
-				
-				
-				this.ent.getGrille().clear();
-				this.ai.data.reset();
-				this.ent.setTourJeu(Joueur.values()[0]);
-				this.entToIhm();
+			this.jouerCoup(id);
+			
+			if(this.verifierFinDePartie())
 				return;
-			}
-			
-			
 			
 			if(this.ent.getMode() == Mode.P_VS_AI)
 			{
-				save = new Grille(this.ent.getGrille());
-				this.aiPlays();
-				this.ai.data.coupsY.add(new Ai.Data.Coup(save, new Grille(this.ent.getGrille())));
-				this.incrementerTourDeJeu();
-				this.entToIhm();
-				vainqueur = this.ent.getGrille().finDePartie();
-				partieTerminee = (vainqueur != null) || this.ent.getGrille().is_filled();
-				if(partieTerminee)
-				{
-					if(vainqueur != null)
-					{
-						System.out.println("Le vainqueur est " + vainqueur.toString());
-						TextFile.stringToFile(this.ai.data.getCoups(vainqueur), 
-								Ai.DATA_DIRPATH + Ai.COUPS_FILENAME, true);
-						this.ai.learn();
-					}
-					else
-						System.out.println("Aucun gagnant");
-					
-					
-					
-					this.ent.getGrille().clear();
-					this.ai.data.reset();
-					this.ent.setTourJeu(Joueur.values()[0]);
-					this.entToIhm();
-				}
+				this.jouerCoup(this.aiPlays());
+				this.verifierFinDePartie();
 			}
 		}	
 	}
 	
-	public void lancerApprentissage() {
+	public void lancerApprentissage() 
+	{
 		Main.learningThread = new Thread(new Apprentissage_Task<>(this.ai));
 		Main.learningThread.setDaemon(true);
 		Main.learningThread.start();
 	}
 	
-	private void aiPlays()
+	private void jouerCoup(int id)
 	{
 		Grille grille = this.ent.getGrille();
+		
+		Grille save = new Grille(grille);
+		grille.set(id, Case.valueOf(this.ent.getTourJeu().toString()));
+		if(this.ent.getTourJeu() == Joueur.X)
+			this.ai.data.coupsX.add(new Ai.Data.Coup(save, new Grille(grille)));
+		else
+			this.ai.data.coupsY.add(new Ai.Data.Coup(save, new Grille(grille)));
+		this.incrementerTourDeJeu();
+		this.entToIhm();
+	}
+	
+	private boolean verifierFinDePartie() throws IOException
+	{
+		Grille grille = this.ent.getGrille();
+		
+		Joueur vainqueur = grille.finDePartie();
+		boolean partieTerminee = (vainqueur != null) || grille.is_filled();
+		if(partieTerminee)
+		{
+			if(vainqueur != null)
+			{
+				System.out.println("Le vainqueur est " + vainqueur.toString());
+				TextFile.stringToFile(this.ai.data.getCoups(vainqueur), 
+						Ai.DATA_DIRPATH + Ai.COUPS_FILENAME, true);
+				this.ai.learn();
+			}
+			else
+				System.out.println("Aucun gagnant");
+			
+			grille.clear();
+			this.ai.data.reset();
+			this.ent.setTourJeu(Joueur.values()[0]);
+			this.entToIhm();
+			
+			return true;
+		}
+		return false;
+	}
+	
+	private int aiPlays()
+	{
+		Grille grille = this.ent.getGrille();
+		
 		double[] input = this.grilleToDoubles(grille);
 		int[] output = this.ai.genOutput(input);
 		int i = 0;
 		while(grille.at(output[i]) != Case.VIDE)
 			++i;
-		grille.set(output[i], Case.O);
+
+		return output[i];
 	}
 	
 	private void incrementerTourDeJeu()
