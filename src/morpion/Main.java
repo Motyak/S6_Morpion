be952c7491789d626b3a16d42bbf8d39456a7d42
Tommy Anfosso <anfossotommy@hotmail.fr>
2020-04-21 00:13:11 +0200

    design plus ou moins OK, slider a revoir

--- src/morpion/Main.java
+++ src/morpion/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
-			Scene scene = new Scene(root,340,340);
+			Scene scene = new Scene(root,630,450);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
